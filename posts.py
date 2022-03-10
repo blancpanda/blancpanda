ec6deab1dc5635d53ed80fa0c67ab58a561394cf
Blancpanda <blancpanda@users.noreply.github.com>
2022-03-10 21:44:36 +0900

    Create posts.py

---
+++ posts.py
@@ -0,0 +1,16 @@
+import feedparser
+import re
+
+feed = feedparser.parse('https://zenn.dev/blancpanda/feed')
+
+dist_md = ''
+
+for entry in feed.entries:
+    dist_md += f'- ![](img/zenn.svg) [{entry.title}]({entry.link})\n'
+
+with open('README.md', 'r') as file:
+    data = file.read()
+
+with open('README.md', 'w') as file:
+    pattern = re.compile(r'<!--\[START POSTS\]-->.*<!--\[END POSTS\]-->', re.DOTALL)
+    file.write(re.sub(pattern, f'<!--[START POSTS]-->\n{dist_md}<!--[END POSTS]-->', data))
